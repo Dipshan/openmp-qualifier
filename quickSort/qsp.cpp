46107851b3960ce8a07b31063550e3b6bf4d1d3e
Sumit <sumitsth21@gmail.com>
2025-10-14 15:25:33 -0500

    fixed qsp.cpp and qss.csp

--- quickSort/qsp.cpp
+++ quickSort/qsp.cpp
@@ -1,15 +1,39 @@
-#include "../common/common.h"
-#include <omp.h>
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <string>
+#include <algorithm> // For std::swap
+#include <cstdlib>   // For std::rand, std::srand
+#include <omp.h>     // Include OpenMP library
 
-int partition(int *array, int low, int high)
-{
+/**
+ * @brief Generates an array of random integers.
+ * @param size The number of elements in the array.
+ * @param seed The seed for the random number generator.
+ * @return A pointer to the dynamically allocated array.
+ */
+int* randNumArray(const int size, const int seed) {
+    std::srand(seed);
+    int* array = new int[size];
+    for (int i = 0; i < size; i++) {
+        array[i] = std::rand() % 1000000;
+    }
+    return array;
+}
+
+/**
+ * @brief Partitions the array for the Quick Sort algorithm.
+ * @param array The array to be partitioned.
+ * @param low The starting index.
+ * @param high The ending index.
+ * @return The partitioning index.
+ */
+int partition(int* array, int low, int high) {
     int pivot = array[high];
     int i = low - 1;
 
-    for (int j = low; j < high; j++)
-    {
-        if (array[j] <= pivot)
-        {
+    for (int j = low; j < high; j++) {
+        if (array[j] <= pivot) {
             i++;
             std::swap(array[i], array[j]);
         }
@@ -18,71 +42,73 @@ int partition(int *array, int low, int high)
     return i + 1;
 }
 
-void quickSort_parallel(int *array, int low, int high)
-{
-    if (low < high)
-    {
+/**
+ * @brief Performs a parallel Quick Sort on an array using OpenMP tasks.
+ * Includes Median-of-Three pivot selection and a task creation threshold.
+ * @param array The array to sort.
+ * @param low The starting index of the portion to sort.
+ * @param high The ending index of the portion to sort.
+ */
+void quickSort_parallel(int* array, int low, int high) {
+    if (low < high) {
+        // --- START: Median-of-Three Pivot Selection ---
+        int mid = low + (high - low) / 2;
+        if (array[mid] < array[low]) {
+            std::swap(array[low], array[mid]);
+        }
+        if (array[high] < array[low]) {
+            std::swap(array[low], array[high]);
+        }
+        if (array[mid] < array[high]) {
+            std::swap(array[mid], array[high]);
+        }
+        // --- END: Median-of-Three Pivot Selection ---
+
         int pi = partition(array, low, high);
 
-        if (high - low > 1000)
-        {   // threshold to avoid too fine-grained tasks
-            // Create parallel tasks for the recursive calls
-#pragma omp task
+        // Threshold to avoid creating tasks for very small subarrays
+        if (high - low > 1000) {
+            #pragma omp task
             {
                 quickSort_parallel(array, low, pi - 1);
             }
-#pragma omp task
+            #pragma omp task
             {
                 quickSort_parallel(array, pi + 1, high);
             }
-#pragma omp taskwait
-        }
-        else
-        {
+        } else {
+            // Sort sequentially if the subarray is small
             quickSort_parallel(array, low, pi - 1);
             quickSort_parallel(array, pi + 1, high);
         }
     }
 }
 
-int main(int argc, char **argv)
-{
-    int size, seed;
-
-    // Per project requirements, only use command-line arguments [cite: 22]
-    if (argc < 3)
-    {
-        std::cerr << "usage: " << argv[0] << " [amount of random nums to generate] [seed value for rand]" << std::endl;
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        std::cerr << "usage: " << argv[0] << " [number of random integers] [seed value]" << std::endl;
         return -1;
     }
 
-    size = std::stoi(argv[1]);
-    seed = std::stoi(argv[2]);
+    int size = std::stoi(argv[1]);
+    int seed = std::stoi(argv[2]);
 
-    int *array = randNumArray(size, seed);
+    int* array = randNumArray(size, seed);
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    // Create a parallel region for the tasks to run in
-#pragma omp parallel
+    #pragma omp parallel
     {
-        // Have a single thread start the initial sort call
-#pragma omp single nowait
+        #pragma omp single nowait
         {
             quickSort_parallel(array, 0, size - 1);
         }
-    }
+    } // Implicit barrier here ensures all tasks are finished.
 
     auto end = std::chrono::high_resolution_clock::now();
-
     std::chrono::duration<double> duration = end - start;
-    // The executable must print the running time [cite: 49]
-    std::cout << "Parallel Quick Sort Time: " << duration.count() << " seconds" << std::endl;
 
-    // Optional: You can add verification if you want
-    // if (!isSorted(array, size)) {
-    //     std::cerr << "Sorting failed!" << std::endl;
-    // }
+    std::cout << duration.count() << std::endl;
 
     delete[] array;
     return 0;
